631e409ee4b5f7afd9e8c366de16a3d0050fc334
Rodrigo Franca <rfrancacosta@gmail.com>
2021-10-03 19:25:43 -0300

    Aula2 Service Registry com Eureka / Aula3 Spring Config Server;

--- loja/src/main/java/br/com/alura/microservice/loja/service/PedidoService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/PedidoService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.service;
 
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,18 +12,27 @@ import br.com.alura.microservice.loja.controller.dto.PedidoDTO;
 @Service
 public class PedidoService {
 	
-	private RestTemplate client;
+	private RestTemplate client;	
+	private DiscoveryClient eurekaClient;
 	
-	public PedidoService(RestTemplate client) {
+	public PedidoService(RestTemplate client, DiscoveryClient eurekaClient) {		
 		this.client = client;
+		this.eurekaClient = eurekaClient;
 	}
 	
 	public void realizarPedido(PedidoDTO pedido) {
+		
 		String estado = pedido.getEndereco().getEstado();
-		String url = "http://ms-fornecedor/api/fornecedor/dados/" + estado; 
-		// RestTemplate client = new RestTemplate();
+		String url = "http://ms-fornecedor/api/fornecedor/dados/" + estado;
+		
 		ResponseEntity<DadosDoFornecedorDTO> response = client.exchange(url, HttpMethod.GET, null, DadosDoFornecedorDTO.class);
+		
+		eurekaClient.getInstances("ms-fornecedor").stream().forEach(f->{
+			System.out.println("http://localhost: " + f.getPort());
+		});
+		
 		System.out.println(response.getBody().getEndereco());
+				
 	}
 
 }
